2928b2eb304705393717ed9b5e6774dd6249c110
Jihoon Seo <jihoon416@snu.ac.kr>
2022-12-13 22:59:02 +0900

    feat: SSR 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import * as React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+    const [count, setCount] = useState(0)
+
+    const handleDecrement = () => {
+        setCount((count) => count - 1)
+    }
+
+    const handleIncrement = () => {
+        setCount((count) => count + 1)
+    }
+
     return (
-        <h1>Hello, World!</h1>
+        <>
+            <button onClick={handleDecrement}>-1</button>
+            <span>  {count}  </span>
+            <button onClick={handleIncrement}>+1</button>
+        </>
     )
 }
 
