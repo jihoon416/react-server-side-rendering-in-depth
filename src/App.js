4e3696a9049b8b8f887f900af85f8abd6051b719
Jihoon Seo <jihoon416@snu.ac.kr>
2022-12-13 23:33:40 +0900

    temp: SSR에서 다른 렌더링

--- src/App.js
+++ src/App.js
@@ -12,11 +12,16 @@ const App = () => {
         setCount((count) => count + 1)
     }
 
+    if (typeof window === 'undefined') {
+        return <h1>Loading...</h1>
+    }
+
     return (
         <>
             <button onClick={handleDecrement}>-1</button>
             <span>  {count}  </span>
             <button onClick={handleIncrement}>+1</button>
+            <h1>My Name is {window.localStorage.getItem('My Name')}</h1>
         </>
     )
 }
